f867d5784a4701652c22c2d6f2d20bc4f97b1334
Anggi Pohontu <anggipohontu378@gmail.com>
2024-09-23 21:50:08 +0800

    refactor: add new column in table users

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	configs "qhealth/app/drivers"
+	"qhealth/app/routes"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	db := configs.InitDB()
+	routes.Routes(e, db)
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodHead,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodPost,
+			http.MethodDelete,
+		},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+			"ngrok-skip-browser-warning",
+		},
+		AllowCredentials: true,
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge: 86400,
+	}))
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}\n",
+	}))
+
+	e.Logger.Fatal(e.Start(":8089"))
+	//e.Logger.Fatal(e.StartTLS(fmt.Sprintf(":%s", "443"), "/etc/letsencrypt/live/tosale.id/fullchain.pem", "/etc/letsencrypt/live/qhealth.id/privkey.pem"))
+}
